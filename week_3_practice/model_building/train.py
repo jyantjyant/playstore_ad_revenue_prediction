8b169842384891505964b86ff8d16493983bbd0d
jyantjyant <jyantjyant@gmail.com>
2026-03-21 10:08:28 +0530

    train

--- week_3_practice/model_building/train.py
+++ week_3_practice/model_building/train.py
@@ -91,8 +91,12 @@ with mlflow.start_run():
     y_pred_test = best_model.predict(Xtest)
 
     # Metrics
-    train_rmse = mean_squared_error(ytrain, y_pred_train, squared=False)
-    test_rmse = mean_squared_error(ytest, y_pred_test, squared=False)
+    # train_rmse = mean_squared_error(ytrain, y_pred_train, squared=False)
+    # test_rmse = mean_squared_error(ytest, y_pred_test, squared=False)
+    from sklearn.metrics import root_mean_squared_error
+
+    train_rmse = root_mean_squared_error(ytrain, y_pred_train)
+    test_rmse = root_mean_squared_error(ytest, y_pred_test)
 
     train_mae = mean_absolute_error(ytrain, y_pred_train)
     test_mae = mean_absolute_error(ytest, y_pred_test)
