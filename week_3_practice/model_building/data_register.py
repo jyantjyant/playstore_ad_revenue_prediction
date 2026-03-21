38ca5dd5ac2bc182d273975f7aa39cbff7e1d21b
jyantjyant <jyantjyant@gmail.com>
2026-03-21 09:45:25 +0530

    data

--- week_3_practice/model_building/data_register.py
+++ week_3_practice/model_building/data_register.py
@@ -3,7 +3,7 @@ from huggingface_hub import HfApi, create_repo
 import os
 
 
-repo_id = "jyantjyant/play-store-revenue-analysis"
+repo_id = "Jyant/play-store-revenue-analysis"
 repo_type = "dataset"
 
 # Initialize API client
